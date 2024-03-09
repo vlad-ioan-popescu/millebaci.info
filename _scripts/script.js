4278fcf8d7ff28e6151912e5409b770f5156a9d0
Vlad Popescu <vladpopescu2004@gmail.com>
2024-03-09 19:47:10 +0200

    menu pages and other refinements

--- _scripts/script.js
+++ _scripts/script.js
@@ -1,42 +1,86 @@
-// intersection observer la header
-const header = document.querySelector('.hero');
-const nav = document.querySelector('.desktop-nav');
-const logo = document.getElementById('logo');
+// on document load...
 
-const headerObserver = new IntersectionObserver(
-	function (entries) {
-		const [entry] = entries;
-		if (!entry.isIntersecting) {
-			nav.classList.add('faded');
-			logo.setAttribute('src', '_img/logo_dark.png');
-		} else {
-			nav.classList.remove('faded');
-			logo.setAttribute('src', '_img/logo.png');
+$(document).ready(function () {
+	// intersection observer la header
+	const header = document.querySelector('.hero');
+
+	const nav = document.querySelector('.desktop-nav');
+
+	const logo = document.getElementById('logo');
+
+	const headerObserver = new IntersectionObserver(
+
+		function (entries) {
+
+			const [entry] = entries;
+
+			if (!entry.isIntersecting) {
+				nav.classList.add('faded');
+
+				logo.setAttribute('src', '_img/logo_dark.png');
+			}
+			else {
+
+				nav.classList.remove('faded');
+
+				logo.setAttribute('src', '_img/logo.png');
+			}
+		},
+		{
+			root: null,
+			threshold: 0,
+			rootMargin: `-50px`,
 		}
-	},
-	{
-		root: null,
-		threshold: 0,
-		rootMargin: `-50px`,
-	}
-);
+	);
 
-headerObserver.observe(header);
+	headerObserver.observe(header);
 
-// arata numarul la CTA
-let oldTel = document.querySelector('.cta').innerText;
-document.querySelector('.cta').addEventListener('mouseover', function (e) {
-	const tel = e.target.getAttribute('href').substring(4).replace('-', '.');
-	e.target.innerText = tel;
+	// arata numarul la hover la CTA
+	let oldTel = document.querySelector('.cta').innerText;
+
+	document.querySelector('.cta').addEventListener('mouseover', function (e) {
+	
+		const tel = e.target.getAttribute('href').substring(4).replace('-', '.');
+	
+		e.target.innerText = tel;
+	});
+
+	document.querySelector('.cta').addEventListener('mouseout', function (e) {
+		
+		e.target.innerText = oldTel;
+	});
+
+	// schimbare categorii la meniuri
+	$('.menu-category').on('click', function() {
+		
+			removeCurrent();
+
+            $(this).addClass('current');
+
+            $('.bottom-left').attr('src','_img/placeholder_' + this.dataset.category + '.png');
+            
+            setTimeout(
+                () => {
+                    $('[data-menufor="' + this.dataset.category + '"]').fadeIn(300);
+
+                    $('.bottom-left').fadeIn(300);
+                }
+                , 400);
+		})
+	
+
+	// sageata de scroll to top
+	window.onscroll = function () {
+		
+        scrollFunction();
+	};
 });
-document.querySelector('.cta').addEventListener('mouseout', function (e) {
-	e.target.innerText = oldTel;
-});
 
-// sageata de scroll to top
-window.onscroll = function () {
-	scrollFunction();
-};
+function removeCurrent() {
+    $('.menu-category').removeClass('current');
+    $('.bottom-left').hide();
+    $('.submenu').fadeOut(300);
+}
 
 function scrollFunction() {
 	if (
